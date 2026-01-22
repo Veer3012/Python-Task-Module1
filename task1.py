98ee27da0aa789f924528163624cf603733b7270
Veer3012 <veer.thakkar2005.vt@gmail.com>
2026-01-22 17:45:13 +0530

    Implement basic arithmetic operations in task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+##problem statement-1
+a = int(input ("Enter the first number: "))
+b = int(input ("Enter the second number: "))
+sum = a + b
+print("Addition:", sum)
+sub = a - b
+print("Subtraction:", sub)
+mul = a * b
+print("Multiplication:", mul)
+div = a / b
+print("Division:", div)
